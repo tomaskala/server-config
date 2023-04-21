c9f0a7deb296dd8a7884854f545ef5a7fa292d43
Tomas Kala <me@tomaskala.com>
2023-04-21 23:53:04 +0200

    Package the unbound blocker

---
+++ nixos/pkgs/unbound-blocker/default.nix
@@ -0,0 +1,13 @@
+{ buildPythonApplication
+, setuptools-scm
+, requests
+}:
+
+buildPythonApplication {
+  pname = "unbound-blocker";
+  version = "0.1.0";
+  src = ../../../src/unbound_blocker;
+  format = "pyproject";
+  nativeBuildInputs = [ setuptools-scm ];
+  propagatedBuildInputs = [ requests ];
+}
