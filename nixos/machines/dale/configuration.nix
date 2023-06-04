aadf33c710362b8a895d49068252de0d62ee57f4
Tomas Kala <tomas.kala@blindspot.ai>
2023-06-04 19:57:57 +0200

    Move nginx hsts-scheme mapping to common HTTP config

--- nixos/machines/dale/configuration.nix
+++ nixos/machines/dale/configuration.nix
@@ -231,9 +231,6 @@ in {
         extraConfig = ''
           # Add HSTS header with preloading to HTTPS requests.
           # Adding this header to HTTP requests is discouraged.
-          map $scheme $hsts_header {
-            https "max-age=31536000; includeSubdomains; preload";
-          }
           add_header Strict-Transport-Security $hsts_header;
 
           # Enable CSP for your services.
--- nixos/services/nginx.nix
+++ nixos/services/nginx.nix
@@ -12,6 +12,12 @@
     commonHttpConfig = ''
       # Disable directory listing.
       autoindex off;
+
+      # Add HSTS header with preloading to HTTPS requests.
+      # Adding this header to HTTP requests is discouraged.
+      map $scheme $hsts_header {
+        https "max-age=31536000; includeSubdomains; preload";
+      }
     '';
   };
 }
