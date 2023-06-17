4b17169b1435bf31e93cd04662bb908695dd3001
Tomas Kala <tomas.kala@blindspot.ai>
2023-06-17 16:54:11 +0200

    Configure Bob's SSH listen addresses

--- machines/bob/configuration.nix
+++ machines/bob/configuration.nix
@@ -3,8 +3,11 @@
 let
   hostName = "bob";
   wanInterface = "eth0";
+
+  intranetCfg = config.networking.intranet;
 in {
-  imports = [ ./secrets-management.nix ../../services/openssh.nix ];
+  imports =
+    [ ./secrets-management.nix ../intranet.nix ../../services/openssh.nix ];
 
   config = {
     system.stateVersion = "23.05";
@@ -60,7 +63,10 @@ in {
       enable = true;
       ports = [ 22 ];
       listenAddresses = [
-        # TODO
+        { addr = intranetCfg.gateways."${hostName}".gateway.ipv4; }
+        { addr = intranetCfg.gateways."${hostName}".gateway.ipv6; }
+        { addr = intranetCfg.gateways."${hostName}".address.ipv4; }
+        { addr = intranetCfg.gateways."${hostName}".address.ipv6; }
       ];
     };
 
