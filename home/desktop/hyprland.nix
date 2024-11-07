3619f68ec3df1ee805c7625209c4ee4ed54d2b8d
Tomas Kala <me@tomaskala.com>
2024-11-07 15:18:56 +0100

    cooper: Configure touchpad

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -46,6 +46,13 @@
         follow_mouse = 2;
         repeat_rate = 50;
         repeat_delay = 300;
+
+        touchpad = {
+          disable_while_typing = false;
+          natural_scroll = true;
+          scroll_factor = 0.6;
+          clickfinger_behavior = true;
+        };
       };
 
       decoration = { rounding = 8; };
