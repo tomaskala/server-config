91581dd98660408409b87b442394132b0bb9448f
Tomas Kala <me@tomaskala.com>
2024-11-07 15:26:16 +0100

    Remove obsolete SSH_AUTH_SOCK

--- home/fish.nix
+++ home/fish.nix
@@ -7,7 +7,6 @@
     interactiveShellInit = ''
       set -gx EMAIL me@tomaskala.com
       set -gx EDITOR nvim
-      set -gx SSH_AUTH_SOCK ~/.ssh/agent.sock
 
       set -gx XDG_CACHE_HOME ~/.cache
       set -gx XDG_CONFIG_HOME ~/.config
