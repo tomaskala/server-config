ac51fabb29cbdc242085022975701108f9b2a4ea
Tomas Kala <me@tomaskala.com>
2024-11-07 16:33:36 +0100

    Configure hyprlock

--- home/desktop/hyprlock.nix
+++ home/desktop/hyprlock.nix
@@ -1,6 +1,14 @@
 { lib, pkgs, ... }:
 
-{
+let
+  text = "#cad3f5";
+  subtext = "#b8c0e0";
+  dark = "#1e2030";
+  bg = "#24273a";
+
+  black = "#181926";
+  purple = "#c6a0f6";
+in {
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -8,6 +16,37 @@
         grace = 5;
         hide_cursor = true;
       };
+
+      background = [{ color = bg; }];
+
+      input-field = [{
+        size = "250, 60";
+        outer_color = "rgb(${black})";
+        inner_color = "rgb(${dark})";
+        font_color = "rgb(${purple})";
+        placeholder_text = "";
+      }];
+
+      label = [
+        {
+          text = "Hello, friend";
+          color = "rgba(${text}, 1.0)";
+          font_size = 64;
+          text_align = "center";
+          halign = "center";
+          valign = "center";
+          position = "0, 160";
+        }
+        {
+          text = "$TIME";
+          color = "rgba(${subtext}, 1.0)";
+          font_size = 32;
+          text_align = "center";
+          halign = "center";
+          valign = "center";
+          position = "0, 75";
+        }
+      ];
     };
   };
 
