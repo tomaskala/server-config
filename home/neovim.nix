e01036cccfd2104c11c00efb8764bd75228fc84b
Tomas Kala <me@tomaskala.com>
2025-02-04 15:42:47 +0100

    neovim: Open floating nvim-tree

--- home/neovim.nix
+++ home/neovim.nix
@@ -160,8 +160,41 @@
         config = # lua
           ''
             do
+              local height_ratio = 0.8
+              local width_ratio = 0.5
+
               require("nvim-tree").setup({
                 update_focused_file = { enable = true },
+                view = {
+                  float = {
+                    enable = true,
+                    open_win_config = function()
+                      local screen_w = vim.opt.columns:get()
+                      local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+
+                      local window_w = screen_w * width_ratio
+                      local window_h = screen_h * height_ratio
+
+                      local window_w_int = math.floor(window_w)
+                      local window_h_int = math.floor(window_h)
+
+                      local center_x = (screen_w - window_w) / 2
+                      local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+
+                      return {
+                        border = "rounded",
+                        relative = "editor",
+                        row = center_y,
+                        col = center_x,
+                        width = window_w_int,
+                        height = window_h_int,
+                      }
+                    end,
+                  },
+                  width = function()
+                    return math.floor(vim.opt.columns:get() * width_ratio)
+                  end,
+                },
                 on_attach = function(bufnr)
                   local api = require("nvim-tree.api")
                   local opts = { buffer = bufnr, noremap = true, silent = true, nowait = true }
