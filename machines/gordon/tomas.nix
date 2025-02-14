5f06ea172d227aa15d101724617bd12adc16f039
Tomas Kala <me@tomaskala.com>
2025-02-14 09:48:31 +0100

    ghostty: Invert cursor bg and fg colors

--- machines/gordon/tomas.nix
+++ machines/gordon/tomas.nix
@@ -24,8 +24,11 @@
 
     file."${config.home.homeDirectory}/.config/ghostty/config".text = ''
       theme = dark:catppuccin-macchiato,light:catppuccin-latte
+      cursor-invert-fg-bg = true
+
       command = ${lib.getExe pkgs.fish}
       macos-icon = retro
+
       # The size gets clamped to the screen size, so this maximizes new windows.
       window-width = 10000
       window-height = 10000
