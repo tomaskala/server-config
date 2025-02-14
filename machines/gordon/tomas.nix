b04a82a7677e600419a6a9833970ab6b462578e1
Tomas Kala <me@tomaskala.com>
2025-02-14 01:00:02 +0100

    ghostty: Start maximized

--- machines/gordon/tomas.nix
+++ machines/gordon/tomas.nix
@@ -26,6 +26,9 @@
       theme = dark:catppuccin-macchiato,light:catppuccin-latte
       command = ${lib.getExe pkgs.fish}
       macos-icon = retro
+      # The size gets clamped to the screen size, so this maximizes new windows.
+      window-width = 10000
+      window-height = 10000
     '';
   };
 }
